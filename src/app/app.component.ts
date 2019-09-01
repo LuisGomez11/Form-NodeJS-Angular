651f7f7ac7c58c3743e700f760b76b886f09f08a
LuisGomez11 <luisgomez24g@gmail.com>
2019-08-31 20:34:10 -0500

    modificacion formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   name = 'Luis Alberto Gomez';
 
-  constructor(){}
+  public formulario: FormGroup;
+
+  nombre:String;
+  correo:String;
+  telefono:String;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  public ngOnInit(){
+    this.formulario = this.formBuilder.group({
+      nombre:'',
+      correo:'',
+      telefono:''
+    });
+  }
+
+  public onSubmit(formValue: any){
+    this.nombre = formValue.nombre;
+    this.correo = formValue.correo;
+    this.telefono = formValue.telefono;
+  }
 
 }
